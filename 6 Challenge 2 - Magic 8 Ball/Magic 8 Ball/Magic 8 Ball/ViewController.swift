83b8360ef4639a8eecd83811daf1976b8fb8e130
Razmanugget <rami_s@hotmail.com>
2018-11-11 12:37:31 +0100

    start

---
+++ 6 Challenge 2 - Magic 8 Ball/Magic 8 Ball/Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Rami on 11/11/18.
+//  Copyright © 2018 LyfeBug. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
