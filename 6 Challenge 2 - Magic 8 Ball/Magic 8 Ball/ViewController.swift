9d06ca39d3e21add53bce0c957c5a49f5b09f4e8
Razmanugget <rami_s@hotmail.com>
2018-11-12 11:09:00 +0100

    finished (swift 4.2)

--- 6 Challenge 2 - Magic 8 Ball/Magic 8 Ball/ViewController.swift
+++ 6 Challenge 2 - Magic 8 Ball/Magic 8 Ball/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  var randomNum = 0
+  let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+  
+  @IBOutlet weak var eightBall: UIImageView!
+  
+  @IBAction func startButton(_ sender: Any) {
+    updateBallImage()
+  }
+  
+  func updateBallImage() {
+    randomNum = Int(arc4random_uniform(5))    // this random works below iOS 12
+    eightBall.image = UIImage(named: ballArray[randomNum])
+  }
+  
+  override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+    updateBallImage()
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    updateBallImage()
   }
-
-
+  
 }
 
