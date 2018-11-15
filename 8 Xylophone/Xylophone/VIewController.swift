b436bbaf0142bcb21fed8deaa9a44a8e9b431f2f
Razmanugget <rami_s@hotmail.com>
2018-11-15 14:02:35 +0100

    removed a global variable

--- 8 Xylophone/Xylophone/VIewController.swift
+++ 8 Xylophone/Xylophone/VIewController.swift
@@ -11,17 +11,15 @@ import AVFoundation
 
 class ViewController: UIViewController, AVAudioPlayerDelegate{
   var audioPlayer: AVAudioPlayer!
-  var selectedSoundFileName: String = ""
   let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
   
   @IBAction func notePressed(_ sender: UIButton) {
-    selectedSoundFileName = soundArray[(sender.tag - 1)]
-    playSound()
+    playSound(soundFileName: soundArray[(sender.tag - 1)])
   }
   
   
-  func playSound() {
-    let soundURL = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")
+  func playSound(soundFileName: String) {
+    let soundURL = Bundle.main.url(forResource: soundFileName, withExtension: "wav")
     
     do {
       audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
