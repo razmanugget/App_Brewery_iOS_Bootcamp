79051041308162b22c8f1a5dc4b6136baea119f2
Razmanugget <rami_s@hotmail.com>
2018-11-15 11:17:47 +0100

    added sound to keys

---
+++ 8 Xylophone/Xylophone/VIewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 27/01/2016.
+//  Copyright © 2016 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController, AVAudioPlayerDelegate{
+//  var audioPlayer = AVAudioPlayer()  // test other method
+  
+  var audioPlayer: AVAudioPlayer!
+  
+  
+  @IBAction func notePressed(_ sender: UIButton) {
+    let soundURL = Bundle.main.url(forResource: "note1", withExtension: "wav")
+    
+    do {
+      audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+    }
+    catch {
+      print(error)
+    }
+    audioPlayer.play()
+
+  }
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  
+}
+
