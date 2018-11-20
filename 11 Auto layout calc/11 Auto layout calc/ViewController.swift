cd5e9dc50a6e08a7e5276b482b729786dfbd9266
Razmanugget <rami_s@hotmail.com>
2018-11-20 17:25:21 +0100

    initial commit

---
+++ 11 Auto layout calc/11 Auto layout calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  11 Auto layout calc
+//
+//  Created by Rami on 11/20/18.
+//  Copyright © 2018 LyfeBug. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
